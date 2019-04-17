a5c4e6a6fcbeeb9920df208eced0ce952298875c
Kevin Homan <kevin@homan.me>
2019-04-17 14:55:32 -0400

    Add project files.

---
+++ MyMealPlanMobile/MyMealPlanMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyMealPlanMobile.Models;
+
+namespace MyMealPlanMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
